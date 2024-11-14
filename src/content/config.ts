e1cb116f89f29b1885f4da2f2cee74c2a2766996
pingfury <pingfury@outlook.com>
2024-11-14 13:08:52 +0800

    fix type

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,16 @@
-import { z, defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 
 const episodeSchema = z.object({
-    title: z.string(),
-    audioUrl: z.string(),
-    pubDate: z.coerce.date().optional(),
-    cover: z.string().optional(),
-    explicit: z.boolean().optional(),
-    episode: z.number().optional(),
-    season: z.number().optional(),
-    episodeType: z.string().optional(),
-    duration: z.coerce.string(), //duration in format hh:mm:ss
-    size: z.number(), // size in megabytes
+  title: z.string(),
+  audioUrl: z.string(),
+  pubDate: z.coerce.date().optional(),
+  cover: z.string().optional(),
+  explicit: z.boolean().optional(),
+  episode: z.any().optional(),
+  season: z.number().optional(),
+  episodeType: z.string().optional(),
+  duration: z.coerce.string(), //duration in format hh:mm:ss
+  size: z.number(), // size in megabytes
 });
 
 export type episodeSchema = z.infer<typeof episodeSchema>;
@@ -18,5 +18,5 @@ export type episodeSchema = z.infer<typeof episodeSchema>;
 const episodeCollection = defineCollection({ schema: episodeSchema });
 
 export const collections = {
-    'episode': episodeCollection,
+  'episode': episodeCollection,
 }
